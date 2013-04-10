89dc38f1e36c150999ad18f9ba08b150a903d516
AlejandroVera <xafilox@gmail.com>
2013-04-10 17:15:52 +0000

    Le he dado formato al código para que sea legible

--- src/shoppingCart/ShoppingCartWSSkeleton.java
+++ src/shoppingCart/ShoppingCartWSSkeleton.java
@@ -1,218 +1,192 @@
-
 /**
  * ShoppingCartWSSkeleton.java
  *
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.5.4  Built on : Dec 19, 2010 (08:18:42 CET)
  */
-    package shoppingCart;
-    /**
-     *  ShoppingCartWSSkeleton java skeleton for the axisService
-     */
-    public class ShoppingCartWSSkeleton{
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.ProductsAmountsList productsInCart
-                  (
-                  
-                  )
-            throws NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#productsInCart");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param productName
-             * @throws ProductUnknownError : 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.ProductAvailableUnits getProductAvailableUnits
-                  (
-                  es.upm.fi.sos.t3.shoppingcart.ProductName productName
-                  )
-            throws ProductUnknownError,NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getProductAvailableUnits");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.Budget budget
-                  (
-                  
-                  )
-            throws NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#budget");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param login
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.LoginResponse login
-                  (
-                  es.upm.fi.sos.t3.shoppingcart.Login login
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#login");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.CostOfCart costOfCart
-                  (
-                  
-                  )
-            throws NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#costOfCart");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.ProductsList getProductsList
-                  (
-                  
-                  )
-            throws NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getProductsList");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param productAmount
-             * @throws ProductUnknownError : 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.ProductAvailable checkProductAvailability
-                  (
-                  es.upm.fi.sos.t3.shoppingcart.ProductAmount productAmount
-                  )
-            throws ProductUnknownError,NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#checkProductAvailability");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param productAmount4
-             * @throws ProductUnknownError : 
-             * @throws NotEnoughUnitsError : 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.ProductAvailableUnits addToCart
-                  (
-                  es.upm.fi.sos.t3.shoppingcart.ProductAmount productAmount4
-                  )
-            throws ProductUnknownError,NotEnoughUnitsError,NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#addToCart");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-         */
-        
-                 public void logout
-                  (
-                  
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-             * @throws NotValidSessionError : 
-             * @throws NotEnoughBudgetError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.Budget buy
-                  (
-                  
-                  )
-            throws NotValidSessionError,NotEnoughBudgetError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#buy");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param productAmount9
-             * @throws ProductUnknownError : 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.ProductAvailableUnits removeFromCart
-                  (
-                  es.upm.fi.sos.t3.shoppingcart.ProductAmount productAmount9
-                  )
-            throws ProductUnknownError,NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#removeFromCart");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param productName11
-             * @throws ProductUnknownError : 
-             * @throws NotValidSessionError : 
-         */
-        
-                 public es.upm.fi.sos.t3.shoppingcart.ProductPrice getProductPrice
-                  (
-                  es.upm.fi.sos.t3.shoppingcart.ProductName productName11
-                  )
-            throws ProductUnknownError,NotValidSessionError{
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getProductPrice");
-        }
-     
-    }
-    
+package shoppingCart;
+
+/**
+ * ShoppingCartWSSkeleton java skeleton for the axisService
+ */
+public class ShoppingCartWSSkeleton {
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.ProductsAmountsList productsInCart(
+
+	) throws NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#productsInCart");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param productName
+	 * @throws ProductUnknownError:
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.ProductAvailableUnits getProductAvailableUnits(
+			es.upm.fi.sos.t3.shoppingcart.ProductName productName)
+			throws ProductUnknownError, NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#getProductAvailableUnits");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.Budget budget(
+
+	) throws NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#budget");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param login
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.LoginResponse login(
+			es.upm.fi.sos.t3.shoppingcart.Login login) {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#login");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.CostOfCart costOfCart(
+
+	) throws NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#costOfCart");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.ProductsList getProductsList(
+
+	) throws NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#getProductsList");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param productAmount
+	 * @throws ProductUnknownError:
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.ProductAvailable checkProductAvailability(
+			es.upm.fi.sos.t3.shoppingcart.ProductAmount productAmount)
+			throws ProductUnknownError, NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#checkProductAvailability");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param productAmount4
+	 * @throws ProductUnknownError:
+	 * @throws NotEnoughUnitsError:
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.ProductAvailableUnits addToCart(
+			es.upm.fi.sos.t3.shoppingcart.ProductAmount productAmount4)
+			throws ProductUnknownError, NotEnoughUnitsError,
+			NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#addToCart");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 */
+
+	public void logout(
+
+	) {
+		// TODO : fill this with the necessary business logic
+
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @throws NotValidSessionError:
+	 * @throws NotEnoughBudgetError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.Budget buy(
+
+	) throws NotValidSessionError, NotEnoughBudgetError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#buy");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param productAmount9
+	 * @throws ProductUnknownError:
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.ProductAvailableUnits removeFromCart(
+			es.upm.fi.sos.t3.shoppingcart.ProductAmount productAmount9)
+			throws ProductUnknownError, NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#removeFromCart");
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param productName11
+	 * @throws ProductUnknownError:
+	 * @throws NotValidSessionError:
+	 */
+
+	public es.upm.fi.sos.t3.shoppingcart.ProductPrice getProductPrice(
+			es.upm.fi.sos.t3.shoppingcart.ProductName productName11)
+			throws ProductUnknownError, NotValidSessionError {
+		// TODO : fill this with the necessary business logic
+		throw new java.lang.UnsupportedOperationException("Please implement "
+				+ this.getClass().getName() + "#getProductPrice");
+	}
+
+}
