ed7a3389b8c1b41513c973918150a2763017d3de
AlejandroVera <xafilox@gmail.com>
2013-04-24 20:17:52 +0000

    Empty commit message

--- startClient.sh
+++ startClient.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+if [ -z $AXIS2_HOME ]; then
+	echo "No se ha definido AXIS2_HOME";
+	exit 1;
+fi
+
 CLASSPATH="$AXIS2_HOME/lib/*:$AXIS2_HOME/repository/modules/addressing-1.5.4.mar:./client/lib/*:./client/bin"
 echo $CLASSPATH
 if [ "$1" = 'test' ]; then
